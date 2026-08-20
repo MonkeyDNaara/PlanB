f5e3e8865ebf868b4e9c6ed94f9ebec50ffb15ce
Firewalldefender <marlinbrubacher04@gmail.com>
2026-08-20 09:46:12 +0200

    headerOutline

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Bock auf Party, PlanB!</h1>
+      <button>Login</button>
+      <button>Home</button>
+    </header>
+  );
+};
+
+export default Header;
