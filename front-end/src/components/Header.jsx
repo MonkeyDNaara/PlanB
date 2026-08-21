6e6e412f79866941b89a0ead218d16e36c46c59d
Firewalldefender <marlinbrubacher04@gmail.com>
2026-08-21 11:32:10 +0200

    minimalisticStyling

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -4,15 +4,17 @@ import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
-    <header>
-      <h1>Bock auf Party, PlanB!</h1>
-      <NavLink to="/login" end>
-        Login
-      </NavLink>
-      <NavLink to="/" end>
-        Home
-      </NavLink>
-      <ThemeToggle />
+    <header className="sticky top-0 z-50 w-full bg-gray-900 border-b border-gray-800 text-gray-100">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
+        <h1>Bock auf Party, PlanB!</h1>
+        <NavLink to="/login" end>
+          Login
+        </NavLink>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <ThemeToggle />
+      </div>
     </header>
   );
 };
