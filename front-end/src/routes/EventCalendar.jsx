1e6a4899c91af4534a29e23179008d65ce3075f5
Firewalldefender <marlinbrubacher04@gmail.com>
2026-08-26 12:30:01 +0200

    react-datepicker

--- front-end/src/routes/EventCalendar.jsx
+++ front-end/src/routes/EventCalendar.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 const EventCalender = () => {
-  return <div>placeholder</div>;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <DatePicker
+      selected={date}
+      onChange={(newDate) => setDate(newDate)}
+      dateFormat="yyyy-MM-dd"
+    />
+  );
 };
 
 export default EventCalender;
